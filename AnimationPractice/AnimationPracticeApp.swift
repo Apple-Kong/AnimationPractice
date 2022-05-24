6049094bb374b6f00807381cc24043af8489e3c4
Apple-Kong <kongthn@gmail.com>
2022-05-24 10:16:17 +0900

    Initial Commit

---
+++ AnimationPractice/AnimationPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationPracticeApp.swift
+//  AnimationPractice
+//
+//  Created by GOngTAE on 2022/05/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimationPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
