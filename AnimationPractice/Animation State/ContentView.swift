04b26ecf790632ac477d45762c3e70eb9ec9cc0a
kongthn <kongthn@gmail.com>
2022-06-14 21:55:10 +0900

    [ADD] 출렁이는 인터랙션 디자인 구현

--- AnimationPractice/Animation State/ContentView.swift
+++ AnimationPractice/Animation State/ContentView.swift
@@ -12,20 +12,21 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            Text("Hello, world!")
-                .bold()
-                .foregroundColor(.white)
-            
-        }
-        .padding()
-        .frame(width: show ? 320 : 300, height: show ? 600 : 44)
-        .background(.blue)
-        .cornerRadius(30)
-        .shadow(color: .blue.opacity(0.5), radius: 20)
-        .onTapGesture {
-            withAnimation(.spring()) {
-                show.toggle()
+            VStack {
+                Text("Hello, world!")
+                    .bold()
+                    .foregroundColor(.white)
             }
+            .padding()
+            .frame(width: show ? 320 : 300, height: show ? 600 : 44)
+            .background(.blue)
+            .cornerRadius(30)
+            .shadow(color: .blue.opacity(0.5), radius: 20)
+            .onTapGesture {
+                withAnimation(.spring()) {
+                    show.toggle()
+                }
+        }
         }
     }
 }
